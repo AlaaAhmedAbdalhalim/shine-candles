427d395d204e1ec84b276b05c2ddb103d953d5c0
unknown <alaaahmedabdalhalim@gmail.com>
2024-06-13 22:07:36 +0300

    create product model

---
+++ src/app/models/products.ts
@@ -0,0 +1,12 @@
+export interface Products {
+    id:number,
+    name:string,
+    img:string,
+    Description:string,
+    price:number,
+    offer:string,
+    quantity:number,
+    categoryId: number, 
+    favStatus:string,
+    soldCount:number
+}
