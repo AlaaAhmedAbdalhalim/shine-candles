fe93bc7c1f17e57ccd0e3ee23aedf59233cdf83c
unknown <alaaahmedabdalhalim@gmail.com>
2024-06-13 22:33:53 +0300

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { MainLayoutComponent } from './components/main-layout/main-layout.component';
+import { BestSellerComponent } from './components/best-seller/best-seller.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { FavoritesComponent } from './components/favorites/favorites.component';
+import { CartComponent } from './components/cart/cart.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,11 @@ import { MainLayoutComponent } from './components/main-layout/main-layout.compon
     FooterComponent,
     HeaderComponent,
     HomeComponent,
-    MainLayoutComponent
+    MainLayoutComponent,
+    BestSellerComponent,
+    CategoriesComponent,
+    FavoritesComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
