929afb8f4e7df70e979fbeba48322ba5742ea932
unknown <alaaahmedabdalhalim@gmail.com>
2024-06-13 22:28:08 +0300

    update routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { MainLayoutComponent } from './components/main-layout/main-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:MainLayoutComponent,children:[
+    {path:'Home',component:HomeComponent,title:'Home Page'},
+   
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+ 
+}
