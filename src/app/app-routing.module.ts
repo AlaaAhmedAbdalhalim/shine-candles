5dc80bd5454a128b37df8815aa06b388f9afe77e
unknown <alaaahmedabdalhalim@gmail.com>
2024-06-13 22:37:16 +0300

    update routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,16 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { MainLayoutComponent } from './components/main-layout/main-layout.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { FavoritesComponent } from './components/favorites/favorites.component';
+import { CartComponent } from './components/cart/cart.component';
 
 const routes: Routes = [
   {path:'',component:MainLayoutComponent,children:[
     {path:'Home',component:HomeComponent,title:'Home Page'},
+    {path:'Categories',component:CategoriesComponent,title:'Categories Page'},
+    {path:'Favorites',component:FavoritesComponent,title:'favorites Page'},
+    {path:'cart',component:CartComponent,title:'cart Page'},
    
   ]}
 ];
