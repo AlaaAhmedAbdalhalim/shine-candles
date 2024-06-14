5b24cf3e7f5356dc50aa36f0a9db88f0eda3165e
unknown <alaaahmedabdalhalim@gmail.com>
2024-06-14 16:43:32 +0300

    update Home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Products } from '../../models/products';
+import { ProductService } from '../../Services/product.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  featuredProducts: Products[] | undefined;
+  constructor(private ProductService :ProductService){ }
 
+  ngOnInit(): void {
+    this.featuredProducts=this.ProductService.getSomeProducts();
+  }
+ 
 }
