a524b5f4f686295f196c0203d8fa00455931c15a
unknown <alaaahmedabdalhalim@gmail.com>
2024-06-14 17:44:28 +0300

    make Categories component

--- src/app/components/categories/categories.component.ts
+++ src/app/components/categories/categories.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../../Services/product.service';
+import { Categories } from '../../models/categories';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-categories',
@@ -6,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrl: './categories.component.css'
 })
 export class CategoriesComponent {
-
+  catList:Categories[];
+  constructor(private productsService: ProductService,
+              private router:Router)
+  {
+    this.catList=productsService.getCat();
+  }
+openDetails(catId:number)
+{
+  this.router.navigate(['../CatDetails',catId]);
+}
 }
