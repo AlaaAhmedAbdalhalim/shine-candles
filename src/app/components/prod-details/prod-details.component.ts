529e9700d53d442b76f3959859224d0535a10087
unknown <alaaahmedabdalhalim@gmail.com>
2024-06-14 17:30:52 +0300

    updating for product details component

--- src/app/components/prod-details/prod-details.component.ts
+++ src/app/components/prod-details/prod-details.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Products } from '../../models/products';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from '../../Services/product.service';
+import { FavouriteService } from '../../Services/favourite.service';
 
 @Component({
   selector: 'app-prod-details',
@@ -6,5 +10,45 @@ import { Component } from '@angular/core';
   styleUrl: './prod-details.component.css'
 })
 export class ProdDetailsComponent {
+  ProdDetailsId : number;
+  inputValue :number = 0;
+  prodDetails :Products |undefined = undefined;
+  sharedValue: any;
 
+
+  
+  constructor (private activatedRouter : ActivatedRoute,
+               private product : ProductService,
+               private router : Router,
+               private favService :FavouriteService)        /* private  sharedService: ProductServiceService */
+        
+{
+    this.ProdDetailsId = Number(this.activatedRouter.snapshot.paramMap.get('prodId') );
+    this.prodDetails = this.product.getProdById(this.ProdDetailsId);
+  }
+/*   ngOnInit() {
+    this.sharedService.sharedValue$.subscribe(value => {
+      this.sharedValue = value;
+    });
+  } */
+  /* Buy(selectedcount:any,ProdDetailsId:any){
+    this.product.sharedData =selectedcount;
+    this.router.navigate(['../YourCart']);
+  } */
+  decrement(){
+    if(this.inputValue>0)
+    {
+      this.inputValue--;
+    }
+   
+  }
+  increment(){
+    if(this.inputValue<this.prodDetails!.quantity)
+      {
+        this.inputValue++;
+      }
+  }
+  addToFavourite(prodId: number) {
+    this.favService.addToFavourite(prodId);
+  }
 }
