09d2a58380c191a20f782a43f88a621def10de4c
unknown <alaaahmedabdalhalim@gmail.com>
2024-06-14 16:47:20 +0300

    update app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
+
 import { HomeComponent } from './components/home/home.component';
 import { MainLayoutComponent } from './components/main-layout/main-layout.component';
 import { CategoriesComponent } from './components/categories/categories.component';
@@ -12,6 +11,7 @@ import { BestSellerComponent } from './components/best-seller/best-seller.compon
 
 const routes: Routes = [
   {path:'',component:MainLayoutComponent,children:[
+    {path:'',redirectTo:'/Home',pathMatch:'full'},
     {path:'Home',component:HomeComponent,title:'Home Page'},
     {path:'Categories',component:CategoriesComponent,title:'Categories Page'},
     {path:'Favorites',component:FavoritesComponent,title:'favorites Page'},
